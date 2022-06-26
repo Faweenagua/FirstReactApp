fef5b6a694ccf1b2987a1909ef0b4b14c5a40e18
Francis Aweenagua <faweenagua@gmail.com>
2022-06-26 18:04:47 +0000

    completed

--- src/components1/Main.js
+++ src/components1/Main.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Main() {
-  return <h1> goat </h1>;
+  return (
+    <main className="main--content">
+      <h1 className="main--heading">Fun facts about React</h1>
+      <ul className="main-list">
+        <li>Was released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousand on Enterprise Apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
 }
--- src/components1/Navbar.js
+++ src/components1/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
-import reactLogo from "../images/react-icon.png";
+import reactLogo from "../images/React-icon.svg.png";
 
 export default function Navbar() {
   return (
     <nav>
       <img src={reactLogo} alt="React Logo" className="nav-icon" />
       <h3 className="nav--logo_text">React Facts</h3>
-      <h4 className="nav_title">React Course - Project 1</h4>
+      <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
   );
 }
